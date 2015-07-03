86056de164ec18f39380506a52d4b22ddddb16e6
John Stovin <john.d.stovin@gmail.com>
2015-07-03 14:51:04 +0100

    Prime factors Kata C# starting point

---
+++ PrimeFactorsKata/Factors.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace PrimeFactorsKata
+{
+    public static class Factors
+    {
+        public static int[] Generate(int n)
+        {
+            return new int[0];
+        }
+    }
+
+    [TestFixture]
+    public class FactorsTest
+    {
+        [Test]
+        public void TestOne()
+        {
+            Assert.AreEqual(new [] {1}, Factors.Generate(1));
+        }
+    }
+}
+
